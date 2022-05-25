f8ddb6aef7ccdb4c2224bedca54d48f787e70a6f
khalil224 <khalilislamju@gmail.com>
2022-05-25 08:35:08 +0600

    index js setup done

---
+++ index.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.x4vab.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db('mobileMart').collection('products');
+
+        app.get('/product', async (req, res) => {
+            const query = {};
+            const cursor = productCollection.find(query);
+            const Product = await cursor.toArray();
+            res.send(Product)
+        });
+
+        app.get('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await productCollection.findOne(query);
+            res.send(product)
+        })
+
+        //post
+        app.post('/product', async (req, res) => {
+            const newProduct = req.body;
+            const result = await productCollection.insertOne(newProduct);
+            res.send(result)
+        });
+
+        //update
+        app.put('product/:id', async (req, res) => {
+            const id = req.params.id;
+            const quantity = req.body;
+            console.log(quantity);
+            const query = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    _id: id,
+                    name: item.name,
+                    price: item.price,
+                    description: item.description,
+                    quantity: quantity.quantity,
+                    suppierName: item.suppierName,
+                    img: item.img
+                }
+            }
+            const result = await itemCollection.updateOne(query, updateDoc, options);
+            res.send(result)
+        })
+
+        //delete
+        app.delete('/product/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await productCollection.deleteOne(query);
+            res.send(result);
+
+        })
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+    res.send('The Computing Cafe')
+})
+
+app.listen(port, () => {
+    console.log('The Computing Cafe is running on port', port)
+})
